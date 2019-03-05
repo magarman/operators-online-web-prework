37b007a4b57054173a60648926ef521640d5775c
Mariana Garman <garman.mariana@gmail.com>
2019-03-05 00:17:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,9 @@
 def unsafe?(speed)
-  if #{speed} < 60
-   return = true
-   elsif #{speed} > 60
-   return = false 
-   
- end
+if speed < 40 || speed > 60
+  return true
+else
+  return false
+end
 end
 
 def not_safe?(speed)
