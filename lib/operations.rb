87c4152ccc0e2b724e25894b690c520030fef3fe
Mariana Garman <garman.mariana@gmail.com>
2019-03-05 00:27:39 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,13 @@
 def unsafe?(speed)
-  
+  if (speed) > 60 || (speed) < 35
+    return true
+  elsif (speed) < 35 || (spped) > 60
+  return false
+  end
 end
+
 def not_safe?(speed)
-	
+  
 end
 	
 
